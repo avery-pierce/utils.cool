e230ef31e17ffae2572fc120d33fb10d829dca9e
Avery Pierce <avery.pierce@pm.me>
2021-10-07 15:04:46 -0500

    Add lap functionality to stopwatch

--- src/pages/timer.js
+++ src/pages/timer.js
@@ -5,15 +5,26 @@ import useCurrentDate from "../hooks/useCurrentDate"
 const Timer = () => {
   const [startDate, setStartDate] = useState(null)
   const [stopDate, setStopDate] = useState(null)
+  const [laps, setLaps] = useState([])
   
   const elapsed = useSecondsElapsed(startDate, stopDate)
   const onStart = useCallback(() => {
     setStartDate(new Date()) 
     setStopDate(null)
+    setLaps([])
   }, [])
 
   const onStop = useCallback(() => {
     setStopDate(new Date())
+  }, [])
+
+  const onTapLap = useCallback(() => {
+    setLaps((prevLaps) => {
+      return [
+        ...prevLaps,
+        new Date()
+      ]
+    })
   }, [])
 
   const isTimerRunning = (!!startDate && !stopDate)
@@ -23,11 +34,24 @@ const Timer = () => {
       <h1>Timer</h1>
       <div>
         {!isTimerRunning && <button onClick={onStart}>Start</button>}
+        {isTimerRunning && <button onClick={onTapLap}>Lap</button>}
         {isTimerRunning && <button onClick={onStop}>Stop</button>}
       </div>
       <p>{elapsed} seconds elapsed</p>
+      <ol>
+        {laps.map(lapTime => (
+          <li>
+            {deltaS(startDate, lapTime)}s
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
+}
+
+function deltaS(date1, date2) {
+  const deltams = Math.abs(date1.getTime() - date2.getTime())
+  return deltams / 1000
 }
 
 function useSecondsElapsed(startDate, stopDate) {
