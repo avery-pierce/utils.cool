37b9bef4341e9adf1e8fabadf51de2145dcbcf00
Avery Pierce <avery.pierce@pm.me>
2021-10-12 21:04:29 -0500

    Add linter

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "settings": {
+        "react": {
+            "version": "detect",
+        },
+    },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
