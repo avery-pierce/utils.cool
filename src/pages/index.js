58cb10dd4993fc78cf73c8bb500ea52b9bd7ab97
Avery Pierce <avery.pierce@pm.me>
2021-10-12 21:50:27 -0500

    redesign home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import Layout from '../components/Layout'
+import CoolText, { CoolTitle } from '../components/CoolText';
+import { Link } from 'gatsby'
 
-const Home = ({location}) => {
+const Home = () => {
   return (
-    <Layout>
-      <h1>Home Page <span>{location.pathname}</span></h1>
-      <h2>Static HTML Page</h2>
-    </Layout>
+    <div style={{
+      padding: "1em",
+      maxWidth: 400,
+      margin: "auto",
+    }}>
+      <CoolTitle style={{ textAlign: "center", }}>UTILS.COOL</CoolTitle>
+      <CoolLink to='/timer'>TIMER</CoolLink>
+    </div>
+  )
+}
+
+function CoolLink({ to, children }) {
+  return (
+    <Link style={{ textDecoration: "none" }} to={to}>
+      <div style={{
+        background: "rgba(233, 233, 233, 0.65)",
+        padding: "0.8em",
+        borderRadius: 12,
+        textAlign: "center",
+        boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)" ,
+      }}>
+        <CoolText bold style={{ 
+          fontSize: "1.4em", 
+          color: "black",
+          textShadow: "0px 0.5px 0px #FFFFFF",
+        }}>{children}</CoolText>
+      </div>
+    </Link>
   )
 }
 
