e7de30862d3648d9e416c787f382fedbd7664a16
Avery Pierce <avery.pierce@pm.me>
2021-10-15 10:55:33 -0500

    Improve timezone page

--- src/pages/timezone.tsx
+++ src/pages/timezone.tsx
@@ -1,15 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import CoolText from "../components/CoolText"
 import useSystemClock, { ClockPrecision } from "../hooks/useSystemClock"
 
+const CURRENT_TIMEZONE = Intl.DateTimeFormat([]).resolvedOptions().timeZone
+
 export default function Timezone() {
   const systemDate = useSystemClock(ClockPrecision.SECOND)
-  const timezoneName = Intl.DateTimeFormat().resolvedOptions().timeZone
+  const [alternateTimeZone, setAlternateTimeZone] = useState("America/Los_Angeles")
+  const localTime = timeInZone(CURRENT_TIMEZONE, systemDate)
+  const alternateTime = timeInZone(alternateTimeZone, systemDate)
 
   return (
     <>
-      <div><CoolText>The current time is {systemDate.toLocaleString()}</CoolText></div>
-      <div><CoolText>{timezoneName}</CoolText></div>
+      <div><CoolText>Your time ({CURRENT_TIMEZONE}) is {localTime}</CoolText></div>
+      <div><CoolText>The time in {alternateTimeZone} is {alternateTime}</CoolText></div>
     </>
   )
+}
+
+function timeInZone(timeZone: string, date: Date) {
+  return Intl.DateTimeFormat([], { timeZone, timeStyle: "full" }).format(date)
 }
